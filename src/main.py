00e213b6f7ff70603e0870ee77908c9c1bab4b6b
simon elenbrink <simonelenbrink@gmail.com>
2025-12-18 13:49:47 +0100

    Grafer

--- src/main.py
+++ src/main.py
@@ -1,5 +1,32 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-
+# Läs in data från csv filen
 df = pd.read_csv("data/heart.csv")
-print(df.head())
+
+# inspektion av datan 
+print(df.info())
+print("\nSaknade värden per kolumn:")
+print(df.isna().sum())
+
+# Tar bort tomma rader ifall det finns
+df = df.dropna() 
+
+print("Shape efter rensning:", df.shape)
+
+# EDA grafer
+
+# hur många som har hjärtsjukdom eller inte
+sns.countplot(x="target", data=df)
+plt.title("Fördelning av hjärtsjukdom")
+plt.show()
+
+# åldersfördelning på datan
+plt.figure(figsize=(6,4))
+sns.histplot(df["age"], bins=20)
+plt.title("Åldersfördelning")
+plt.xlabel("Ålder")
+plt.ylabel("Antal")
+
+plt.show()
